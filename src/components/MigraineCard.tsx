9541a296555e8843ce7b678b7b016012f7fb2b61
Tatsiana Hladkaya <tatyana.hladkaya@gmail.com>
2026-09-07 22:19:48 +0300

    style: change icons placement

--- src/components/MigraineCard.tsx
+++ src/components/MigraineCard.tsx
@@ -22,14 +22,14 @@ export function MigraineCard({ migraine, onDelete, onEdit }: MigraineCardProps) 
       <div>
         <button
           type="button"
-          className="absolute right-0 bottom-0 rounded-br-xl p-4 flex items-center justify-center bg-blue-100 hover:bg-blue-200 h-1/2 hover:cursor-pointer"
+          className="absolute right-0 top-0 rounded-tr-xl p-4 flex items-center justify-center bg-blue-100 hover:bg-blue-200 h-1/2 hover:cursor-pointer"
           onClick={onEdit}
         >
           <img className="w-3 h-3" src="/edit.png" alt="edit migraine" />
         </button>
         <button
           type="button"
-          className="absolute right-0 top-0 rounded-tr-xl p-4 flex items-center justify-center bg-purple-100 hover:bg-purple-200 h-1/2 hover:cursor-pointer"
+          className="absolute right-0 bottom-0 rounded-br-xl p-4 flex items-center justify-center bg-purple-100 hover:bg-purple-200 h-1/2 hover:cursor-pointer"
           onClick={onDelete}
         >
           <img className="w-3 h-3" src="/icon-trash-can.png" alt="delete migraine" />
